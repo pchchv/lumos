e1581b471d3c6a55879cf4acae8cd03c07d55e67
pchchv <ipchchv@gmail.com>
2024-04-02 15:00:38 +0300

    Implement an onmessage worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,2 +1,21 @@
 import * as StellarSdk from '@stellar/stellar-sdk';
 
+var onmessage = function(e: MessageEvent) {
+    var suffix = e.data;
+
+    console.log("Generation started at " + new Date());
+
+    var pair = StellarSdk.Keypair.random();
+    var st = new Date().getTime();
+    while (true) {
+        if (pair.publicKey().endsWith(("" + suffix).toUpperCase())) {
+            break;
+        }
+        pair = StellarSdk.Keypair.random();
+    }
+    var end =  new Date().getTime();
+
+    console.log("Generation completed after " + (end-st) + "ms");
+    
+    postMessage([pair.secret(), pair.publicKey(), (end-st)]);
+}
