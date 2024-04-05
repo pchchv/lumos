70afc843cb3ea8adb06751ed49b664172d00c79a
pchchv <ipchchv@gmail.com>
2024-04-05 15:56:35 +0300

    implement a fallbackMessage

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,16 @@ function clearTooltip(e: MouseEvent) {
   (e.currentTarget as HTMLElement).setAttribute("class", "btn btn-outline-secondary");
   (e.currentTarget as HTMLElement).removeAttribute("aria-label");
 }
+
+function fallbackMessage(action: string): string {
+  let actionMsg = "";
+  const actionKey = action === "cut" ? "X" : "C";
+  if (/iPhone|iPad/i.test(navigator.userAgent)) {
+    actionMsg = "No support :(";
+  } else if (/Mac/i.test(navigator.userAgent)) {
+    actionMsg = `Press ⌘-${actionKey} to ${action}`;
+  } else {
+    actionMsg = `Press Ctrl-${actionKey} to ${action}`;
+  }
+  return actionMsg;
+}
