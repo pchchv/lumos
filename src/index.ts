1bd83587ce4ee8ebdd91291df3300ca72fc1db51
pchchv <ipchchv@gmail.com>
2024-04-06 14:31:10 +0300

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 function showTooltip(elem: HTMLElement, msg: string) {
   elem.setAttribute("aria-label", msg);
-  elem.setAttribute("class", "btn btn-outline-secondary tooltipped tooltipped-s");
+  elem.setAttribute(
+    "class",
+    "btn btn-outline-secondary tooltipped tooltipped-s",
+  );
 }
 
 function clearTooltip(e: MouseEvent) {
-  (e.currentTarget as HTMLElement).setAttribute("class", "btn btn-outline-secondary");
+  (e.currentTarget as HTMLElement).setAttribute(
+    "class",
+    "btn btn-outline-secondary",
+  );
   (e.currentTarget as HTMLElement).removeAttribute("aria-label");
 }
 
