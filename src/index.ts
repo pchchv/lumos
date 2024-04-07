3bc43447cf212dcbae38fb545a79da97ca670b5a
pchchv <ipchchv@gmail.com>
2024-04-07 13:38:31 +0300

    Implement an EventListener(suffix badge)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import ClipboardJS from "clipboard";
+
 function showTooltip(elem: Element, msg: string) {
   elem.setAttribute("aria-label", msg);
   elem.setAttribute(
@@ -26,3 +28,60 @@ function fallbackMessage(action: string): string {
   }
   return actionMsg;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btns = document.querySelectorAll(".btn.btn-outline-secondary");
+  btns.forEach((btn: Element) => {
+    btn.addEventListener("mouseleave", clearTooltip);
+    btn.addEventListener("blur", clearTooltip);
+  });
+
+  const clipboard = new ClipboardJS('.btn.btn-outline-secondary');
+  clipboard.on("success", function (e) {
+    e.clearSelection();
+    showTooltip(e.trigger, "Copied!");
+    console.log(e.trigger);
+  });
+
+  clipboard.on("error", function (e) {
+    showTooltip(e.trigger, fallbackMessage(e.action));
+  });
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    )
+  ) {
+    const mobileAlert = document.getElementById("mobile-alert");
+    if (mobileAlert) {
+      mobileAlert.style.display = "block";
+    }
+  }
+
+  const suffixInput = document.getElementById("suffix") as HTMLInputElement;
+  suffixInput.addEventListener("keyup", function () {
+    if (this.value.length >= 4) {
+      const suffixBadge = document.getElementById("suffixbadge");
+      if (suffixBadge) {
+        suffixBadge.style.visibility = "visible";
+        if (this.value.length === 4) {
+          suffixBadge.classList.remove("badge-danger");
+          suffixBadge.classList.add("badge-warning");
+          suffixBadge.textContent =
+            "Will take 30-120 min to compute addresses";
+        } else {
+          suffixBadge.classList.remove("badge-warning");
+          suffixBadge.classList.add("badge-danger");
+          suffixBadge.textContent =
+            "Will take more than 2 hours to compute addresses";
+        }
+      }
+    } else {
+      const suffixBadge = document.getElementById("suffixbadge");
+      if (suffixBadge) {
+        suffixBadge.style.visibility = "hidden";
+      }
+    }
+  });
+});
+
